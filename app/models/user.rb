90720729443cb559eba951cfed8e920ceebdd4d9
lilywu-JP <wulizxj@yahoo.co.jp>
2024-09-25 21:50:01 +0900

    ユーザー登録機能実装＆単体テスト　修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,11 @@ class User < ApplicationRecord
                        if: -> { encrypted_password.present? }
   validates :nickname, presence: true
   validates :last_name, presence: true,
-                        format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力してください' }
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力してください' }
   validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力してください' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力してください' }
 
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）での入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）での入力してください' }
+  validates :birth_date, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it '正常系テスト' do
+      expect(@user).to be_valid
+    end
+
+    it 'nicknameが必須' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
     it 'メールアドレスが必須' do
       @user.email = ''
       @user.valid?
@@ -37,11 +47,83 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
+    it '英字のみのパスワードでは登録できない' do
+      @user.password = 'abcdef'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
+    end
+
+    it '全角を含むパスワードでは登録できない' do
+      @user.password = 'ａbcdef'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
+    end
+
+    it '数字のみのパスワードでは登録できない' do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
+    end
+
     it 'パスワードとパスワード（確認）は、値の一致が必須' do
       @user.password = '123456'
       @user.password_confirmation = '1234567'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it '苗字が必須' do
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it '名前が必須' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it '苗字（カナ）が必須' do
+      @user.last_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end
+
+    it '名前（カナ）が必須' do
+      @user.first_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+    end
+
+    it '苗字が全角（漢字・ひらがな・カタカナ）での入力が必須' do
+      @user.last_name = 'haak'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Last name 全角（漢字・ひらがな・カタカナ）での入力してください')
+    end
+
+    it '名前が全角（漢字・ひらがな・カタカナ）での入力が必須' do
+      @user.first_name = 'abdg'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name 全角（漢字・ひらがな・カタカナ）での入力してください')
+    end
+
+    it '苗字（カナ）がカナで入力が必須' do
+      @user.last_name_kana = '苗字'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Last name kana 全角（カタカナ）での入力してください')
+    end
+
+    it '名前（カナ）がカナでの入力が必須' do
+      @user.first_name_kana = '名前'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name kana 全角（カタカナ）での入力してください')
+    end
+
+    it '生年月日が必須' do
+      @user.birth_date = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth date can't be blank")
     end
   end
 end
