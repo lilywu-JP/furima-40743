0a4e2b91846f09e2cb73b7b06a4d2947d8c1562f
lilywu-JP <wulizxj@yahoo.co.jp>
2024-09-28 17:11:04 +0900

    ユーザー管理機能単体テスト　修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,65 +5,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-<<<<<<< Updated upstream
-    context '新規登録できるとき' do
-      it '正常系テスト' do
-=======
     context '正常系テスト' do
       it '正常登録' do
->>>>>>> Stashed changes
         expect(@user).to be_valid
       end
     end
-    context '新規登録できないとき' do
-      it 'nicknameが必須' do
-        @user.nickname = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
-      end
 
-<<<<<<< Updated upstream
-      it 'メールアドレスが必須' do
-        @user.email = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
-      end
-
-      it 'メールアドレスが一意性' do
-        @user.save
-        another_user = FactoryBot.build(:user)
-        another_user.email = @user.email
-        another_user.valid?
-        expect(another_user.errors.full_messages).to include('Email has already been taken')
-      end
-
-      it 'メールアドレスは、@を含む必要がある' do
-        @user.email = 'testmail'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Email is invalid')
-      end
-
-      it 'パスワードが必須' do
-        @user.password = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
-      end
-
-      it 'パスワードは、6文字以上での入力が必須' do
-        @user.password = 'a1234'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-      end
-
-      it '英字のみのパスワードでは登録できない' do
-        @user.password = 'abcdef'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
-      end
-
-      it '全角を含むパスワードでは登録できない' do
-        @user.password = 'ａbcdef'
-=======
     context '新規登録できないとき' do
       it 'nicknameが必須' do
         @user.nickname = ''
@@ -105,81 +52,16 @@ RSpec.describe User, type: :model do
 
       it '英字のみのパスワードでは登録できない' do
         @user.password = 'abcdef'
->>>>>>> Stashed changes
         @user.valid?
         expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
       end
 
-<<<<<<< Updated upstream
       it '数字のみのパスワードでは登録できない' do
         @user.password = '123456'
-=======
+      end
+
       it '全角を含むパスワードでは登録できない' do
         @user.password = 'ａbcdef'
->>>>>>> Stashed changes
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
-      end
-
-<<<<<<< Updated upstream
-      it 'パスワードとパスワード（確認）は、値の一致が必須' do
-        @user.password = '123456'
-        @user.password_confirmation = '1234567'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-      end
-
-      it '苗字が必須' do
-        @user.last_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Last name can't be blank")
-      end
-
-      it '名前が必須' do
-        @user.first_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank")
-      end
-
-      it '苗字（カナ）が必須' do
-        @user.last_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
-      end
-
-      it '名前（カナ）が必須' do
-        @user.first_name_kana = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank")
-      end
-
-      it '苗字が全角（漢字・ひらがな・カタカナ）での入力が必須' do
-        @user.last_name = 'haak'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Last name 全角（漢字・ひらがな・カタカナ）での入力してください')
-      end
-
-      it '名前が全角（漢字・ひらがな・カタカナ）での入力が必須' do
-        @user.first_name = 'abdg'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('First name 全角（漢字・ひらがな・カタカナ）での入力してください')
-      end
-
-      it '苗字（カナ）がカナで入力が必須' do
-        @user.last_name_kana = '苗字'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Last name kana 全角（カタカナ）での入力してください')
-      end
-
-      it '名前（カナ）がカナでの入力が必須' do
-        @user.first_name_kana = '名前'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('First name kana 全角（カタカナ）での入力してください')
-      end
-
-=======
-      it '数字のみのパスワードでは登録できない' do
-        @user.password = '123456'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password パスワードは半角英字と数字の両方を含めてください')
       end
@@ -239,7 +121,6 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('First name kana 全角（カタカナ）での入力してください')
       end
 
->>>>>>> Stashed changes
       it '生年月日が必須' do
         @user.birth_date = ''
         @user.valid?
