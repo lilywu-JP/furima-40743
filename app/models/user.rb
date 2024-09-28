d138b0921c232a7c41869653a31921880daadab3
lilywu-JP <wulizxj@yahoo.co.jp>
2024-09-28 16:31:20 +0900

    ユーザー管理機能、単体テスト修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'メールアドレスは、@を含む必要があります' }
-  validates :password, length: { minimum: 6 },
-                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'パスワードは半角英字と数字の両方を含めてください' },
-                       if: -> { encrypted_password.present? }
+  validates :password,
+            format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'パスワードは半角英字と数字の両方を含めてください' },
+            if: -> { encrypted_password.present? }
   validates :nickname, presence: true
   validates :last_name, presence: true,
                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力してください' }
